8896cfbc0a37bdd10e1287105eb58b19b29e1b41
Hiram Ordoñez <hiram.ordonez@go1.mx>
2023-12-16 14:29:18 -0500

    create widget

---
+++ ios/HiramOmeTest/HiramOmeTestBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HiramOmeTestBundle.swift
+//  HiramOmeTest
+//
+//  Created by Hiram Ordoñez on 16/12/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HiramOmeTestBundle: WidgetBundle {
+    var body: some Widget {
+        HiramOmeTest()
+    }
+}
